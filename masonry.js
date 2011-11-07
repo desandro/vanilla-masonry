6ad15b99cdda8c34e6f0ce602db6410247ee19ab
David DeSandro <david@desandro.com>
2011-11-07 11:04:30 -0500

    src : add debounce helper; add resize demo

--- masonry.js
+++ masonry.js
@@ -58,8 +58,7 @@
 
   // -------------------------- addEvent / removeEvent -------------------------- //
 
-  // by John Resig
-  // http://ejohn.org/projects/flexible-javascript-events/
+  // by John Resig - http://ejohn.org/projects/flexible-javascript-events/
 
   function addEvent( obj, type, fn ) {
     if ( obj.addEventListener )
@@ -82,5 +81,37 @@
       obj[ 'e' + type + fn ] = null;
     }
   }
+
+  window.addEvent = addEvent;
+  window.removeEvent = removeEvent;
+
+  // -------------------------- debounce -------------------------- //
+
+  // by John Hann - http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+
+  function debounce( func, threshold, execAsap ) {
+    var timeout;
+
+    return function debounced() {
+      var obj = this, args = arguments;
+      function delayed () {
+        if ( !execAsap ) {
+          func.apply( obj, args );
+        }
+        timeout = null;
+      };
+
+      if ( timeout ) {
+        clearTimeout( timeout );
+      } else if ( execAsap ) {
+        func.apply( obj, args );
+      }
+
+      timeout = setTimeout(delayed, threshold || 100);
+    };
+
+  }
+
+  window.debounce = debounce;
 
 })( window );
